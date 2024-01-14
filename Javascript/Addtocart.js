47133c097a55ff640ffac6f5a5b04b68953031ce
ankitapawar0608 <kedarpawar775@gmail.com>
2024-01-14 18:04:11 +0530

    Add to cart page

---
+++ Javascript/Addtocart.js
@@ -0,0 +1,294 @@
+
+// Toast
+
+function toastclick(){
+    alert("Added to cart successfully")
+}
+
+function inc() {
+    const a = document.getElementById("numchange1")
+    const qnt = parseInt(a.innerHTML)
+    if (qnt == 10) {
+        alert("Maximum Quantity of product is 10")
+    }
+    else {
+        a.innerHTML = qnt + 1;
+    }
+
+}
+
+function dec() {
+    const a = document.getElementById("numchange1")
+    const qnt = parseInt(a.innerHTML)
+    if (qnt == 1) {
+        alert("Minimum Quantity of product is 1")
+    }
+    else {
+        a.innerHTML = qnt - 1;
+    }
+
+}
+
+function inc12() {
+    const a = document.getElementById("numchange12")
+    const qnt = parseInt(a.innerHTML)
+    if (qnt == 10) {
+        alert("Maximum Quantity of product is 10")
+    }
+    else {
+        a.innerHTML = qnt + 1;
+    }
+
+}
+
+function dec12() {
+    const a = document.getElementById("numchange12")
+    const qnt = parseInt(a.innerHTML)
+    if (qnt == 1) {
+        alert("Minimum Quantity of product is 1")
+    }
+    else {
+        a.innerHTML = qnt - 1;
+    }
+
+}
+
+function inc13() {
+    const a = document.getElementById("numchange13")
+    const qnt = parseInt(a.innerHTML)
+    if (qnt == 10) {
+        alert("Maximum Quantity of product is 10")
+    }
+    else {
+        a.innerHTML = qnt + 1;
+    }
+
+}
+
+function dec13() {
+    const a = document.getElementById("numchange13")
+    const qnt = parseInt(a.innerHTML)
+    if (qnt == 1) {
+        alert("Minimum Quantity of product is 1")
+    }
+    else {
+        a.innerHTML = qnt - 1;
+    }
+
+}
+
+function inc14() {
+    const a = document.getElementById("numchange14")
+    const qnt = parseInt(a.innerHTML)
+    if (qnt == 10) {
+        alert("Maximum Quantity of product is 10")
+    }
+    else {
+        a.innerHTML = qnt + 1;
+    }
+
+}
+
+function dec14() {
+    const a = document.getElementById("numchange14")
+    const qnt = parseInt(a.innerHTML)
+    if (qnt == 1) {
+        alert("Minimum Quantity of product is 1")
+    }
+    else {
+        a.innerHTML = qnt - 1;
+    }
+
+}
+
+function inc15() {
+    const a = document.getElementById("numchange15")
+    const qnt = parseInt(a.innerHTML)
+    if (qnt == 10) {
+        alert("Maximum Quantity of product is 10")
+    }
+    else {
+        a.innerHTML = qnt + 1;
+    }
+
+}
+
+function dec15() {
+    const a = document.getElementById("numchange15")
+    const qnt = parseInt(a.innerHTML)
+    if (qnt == 1) {
+        alert("Minimum Quantity of product is 1")
+    }
+    else {
+        a.innerHTML = qnt - 1;
+    }
+
+}
+
+function inc16() {
+    const a = document.getElementById("numchange16")
+    const qnt = parseInt(a.innerHTML)
+    if (qnt == 10) {
+        alert("Maximum Quantity of product is 10")
+    }
+    else {
+        a.innerHTML = qnt + 1;
+    }
+
+}
+
+function dec16() {
+    const a = document.getElementById("numchange16")
+    const qnt = parseInt(a.innerHTML)
+    if (qnt == 1) {
+        alert("Minimum Quantity of product is 1")
+    }
+    else {
+        a.innerHTML = qnt - 1;
+    }
+
+}
+
+
+function inc17() {
+    const a = document.getElementById("numchange17")
+    const qnt = parseInt(a.innerHTML)
+    if (qnt == 10) {
+        alert("Maximum Quantity of product is 10")
+    }
+    else {
+        a.innerHTML = qnt + 1;
+    }
+
+}
+
+function dec17() {
+    const a = document.getElementById("numchange17")
+    const qnt = parseInt(a.innerHTML)
+    if (qnt == 1) {
+        alert("Minimum Quantity of product is 1")
+    }
+    else {
+        a.innerHTML = qnt - 1;
+    }
+
+}
+
+function inc18() {
+    const a = document.getElementById("numchange18")
+    const qnt = parseInt(a.innerHTML)
+    if (qnt == 10) {
+        alert("Maximum Quantity of product is 10")
+    }
+    else {
+        a.innerHTML = qnt + 1;
+    }
+
+}
+
+function dec18() {
+    const a = document.getElementById("numchange18")
+    const qnt = parseInt(a.innerHTML)
+    if (qnt == 1) {
+        alert("Minimum Quantity of product is 1")
+    }
+    else {
+        a.innerHTML = qnt - 1;
+    }
+
+}
+
+function inc19() {
+    const a = document.getElementById("numchange19")
+    const qnt = parseInt(a.innerHTML)
+    if (qnt == 10) {
+        alert("Maximum Quantity of product is 10")
+    }
+    else {
+        a.innerHTML = qnt + 1;
+    }
+
+}
+
+function dec19() {
+    const a = document.getElementById("numchange19")
+    const qnt = parseInt(a.innerHTML)
+    if (qnt == 1) {
+        alert("Minimum Quantity of product is 1")
+    }
+    else {
+        a.innerHTML = qnt - 1;
+    }
+
+}
+
+function inc110() {
+    const a = document.getElementById("numchange110")
+    const qnt = parseInt(a.innerHTML)
+    if (qnt == 10) {
+        alert("Maximum Quantity of product is 10")
+    }
+    else {
+        a.innerHTML = qnt + 1;
+    }
+
+}
+
+function dec110() {
+    const a = document.getElementById("numchange110")
+    const qnt = parseInt(a.innerHTML)
+    if (qnt == 1) {
+        alert("Minimum Quantity of product is 1")
+    }
+    else {
+        a.innerHTML = qnt - 1;
+    }
+
+}
+
+function inc111() {
+    const a = document.getElementById("numchange111")
+    const qnt = parseInt(a.innerHTML)
+    if (qnt == 10) {
+        alert("Maximum Quantity of product is 10")
+    }
+    else {
+        a.innerHTML = qnt + 1;
+    }
+
+}
+
+function dec111() {
+    const a = document.getElementById("numchange111")
+    const qnt = parseInt(a.innerHTML)
+    if (qnt == 1) {
+        alert("Minimum Quantity of product is 1")
+    }
+    else {
+        a.innerHTML = qnt - 1;
+    }
+
+}
+function inc112() {
+    const a = document.getElementById("numchange112")
+    const qnt = parseInt(a.innerHTML)
+    if (qnt == 10) {
+        alert("Maximum Quantity of product is 10")
+    }
+    else {
+        a.innerHTML = qnt + 1;
+    }
+
+}
+
+function dec112() {
+    const a = document.getElementById("numchange112")
+    const qnt = parseInt(a.innerHTML)
+    if (qnt == 1) {
+        alert("Minimum Quantity of product is 1")
+    }
+    else {
+        a.innerHTML = qnt - 1;
+    }
+
+}
